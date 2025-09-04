221128492feb1d385f93b997ed122035b3215abb
longanisainhertaco <az@az4nm.com>
2025-09-03 22:42:40 -0600

    Expose materials utilities and stub in simulation test

--- src/dpf2/materials/__init__.py
+++ src/dpf2/materials/__init__.py
@@ -2,6 +2,15 @@
 """Material-related models and helpers."""
 
 from .models import MaterialRef
+from .library import Material, MaterialLibrary
+from .state import ComponentMaterialState
+from .mdm import MaterialDamageModel
 
-__all__ = ["MaterialRef"]
+__all__ = [
+    "MaterialRef",
+    "Material",
+    "MaterialLibrary",
+    "ComponentMaterialState",
+    "MaterialDamageModel",
+]
 
